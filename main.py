48b90350b3b5f335ee9e5aaf6cec72549fc849ec
Artiya T <artiya4u@gmail.com>
2015-12-02 01:07:53 +0700

    Add detail of word.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 def to_number(word):
     num = 0
     for w in word:
-        value = ord(w)
-        num += (value - 64)
+        value = ord(w) - 64
+        num += value
     return num
+
+
+def detail(word):
+    for w in word:
+        value = ord(w) - 64
+        yield str(value)
+
 
 with open('words.txt') as f:
     for line in f:
         line = line.rstrip().upper()
         if to_number(line) == 100:
-            print(line)
+            print(line + ' = ' + '+'.join(detail(line)))
